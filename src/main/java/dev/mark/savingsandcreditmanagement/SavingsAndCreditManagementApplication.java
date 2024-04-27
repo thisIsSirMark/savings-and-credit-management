c6332bc6e9562dc8f1a6bc6e5e1af5dfa54df672
thisIsSirMark <markjaysonlanuzo9@gmail.com>
2024-04-27 09:43:54 +0800

    init

---
+++ src/main/java/dev/mark/savingsandcreditmanagement/SavingsAndCreditManagementApplication.java
@@ -0,0 +1,15 @@
+package dev.mark.savingsandcreditmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsAndCreditManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsAndCreditManagementApplication.class, args);
+
+		System.out.println("hello world!!!");
+	}
+
+}
---
+++ src/test/java/dev/mark/savingsandcreditmanagement/SavingsAndCreditManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.savingsandcreditmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsAndCreditManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
